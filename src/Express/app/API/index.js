0d65658533a3bcefcae2d85eaa7c0b2c1bdfc7aa
Matheus <Matheus>
2021-02-16 10:18:42 -0300

    Finished POC

--- src/Express/app/API/index.js
+++ src/Express/app/API/index.js
@@ -4,29 +4,34 @@ const ObjectId = require('mongodb').ObjectId;
 
 const Express = require('express')
 const app = Express()
+const cors = require('cors')
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+	res.json({msg: 'Hello World!'})
 })
 
-app.get('/search', (req, res) => {
-  const collection = req.app.locals.collection;
-  const search = {}
-  if (req.query.name 		!== undefined){ search["name"] 				= {$regex: req.query.name} }
-  if (req.query.hp 			!== undefined){ search["stats.hp"] 			= req.query.hp }
-  if (req.query.attack 		!== undefined){ search["stats.attack"] 		= req.query.attack }
-  if (req.query.defense 	!== undefined){ search["stats.defense"] 	= req.query.defense }
-  if (req.query.spattack 	!== undefined){ search["stats.spattack"] 	= req.query.spattack }
-  if (req.query.spdefense 	!== undefined){ search["stats.spdefense"] 	= req.query.spdefense }
-  if (req.query.speed 		!== undefined){ search["stats.speed"] 		= req.query.speed }
-  collection.findOne(search).then(response => res.status(200).json(response)).catch(error => console.error(error));
+app.get('/search', cors(), (req, res) => {
+	const collection = req.app.locals.collection;
+	const search = {}
+	if (req.query.name			!== undefined){ search["name"]						= {$regex: req.query.name, $options: 'i'} }
+	if (req.query.hp				!== undefined){ search["stats.hp"]				= req.query.hp }
+	if (req.query.attack		!== undefined){ search["stats.attack"]		= req.query.attack }
+	if (req.query.defense		!== undefined){ search["stats.defense"]		= req.query.defense }
+	if (req.query.spattack	!== undefined){ search["stats.spattack"]	= req.query.spattack }
+	if (req.query.spdefense	!== undefined){ search["stats.spdefense"]	= req.query.spdefense }
+	if (req.query.speed			!== undefined){ search["stats.speed"]			= req.query.speed }
+	collection.find(search).toArray(function(err, response) {
+    if (err) throw err;
+    res.status(200).json(response);
+  });
+	//collection.findOne(search).then(response => res.status(200).json(response)).catch(error => console.error(error));
 });
 
 MongoClient.connect(mongo_uri, { useNewUrlParser: true })
 .then(client => {
-  const db = client.db('pokemon');
-  const collection = db.collection('pokemons');
-  app.locals.collection = collection;
-  app.listen(port, () => console.info(`REST API running on port ${port}`));
+	const db = client.db('pokemon');
+	const collection = db.collection('pokemons');
+	app.locals.collection = collection;
+	app.listen(port, () => console.info(`REST API running on port ${port}`));
 }).catch(error => console.error(error));
--- src/React/app/src/index.js
+++ src/React/app/src/index.js
@@ -1,228 +1,71 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 import './index.css';
 
-function Square(props) {
+function Status_bar(props){
 	return (
-		<button className="square" onClick={props.onClick}>
-			{props.value}
-		</button>
+		<tr>
+			<td style={{width: "10em"}}>{props.title}: </td>
+			<td style={{width: "3em"}}> {props.value} </td>
+			<td> <div style={{backgroundColor: "#00f", width: `calc(100% * ${props.value}/255)`, height: "15px"}}></div> </td>
+		</tr>
 	);
 }
-
-class Board extends React.Component {
-	renderSquare(i) {
-		return (
-			<Square
-				value={this.props.squares[i]}
-				onClick={() => this.props.onClick(i)}
-			/>
-		);
-	}
-
-	render() {
-		return (
-			<div>
-				<div className="board-row">
-					{this.renderSquare(0)}
-					{this.renderSquare(1)}
-					{this.renderSquare(2)}
-				</div>
-				<div className="board-row">
-					{this.renderSquare(3)}
-					{this.renderSquare(4)}
-					{this.renderSquare(5)}
-				</div>
-				<div className="board-row">
-					{this.renderSquare(6)}
-					{this.renderSquare(7)}
-					{this.renderSquare(8)}
-				</div>
-			</div>
-		);
-	}
-}
-
-class Game extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			history: [{
-				squares: Array(9).fill(null)
-			}],
-			stepNumber: 0,
-			xIsNext: true
-		};
-	}
-
-	handleClick(i) {
-		const history = this.state.history.slice(0, this.state.stepNumber + 1);
-		const current = history[history.length - 1];
-		const squares = current.squares.slice();
-		if (calculateWinner(squares) || squares[i]) {
-			return;
-		}
-		squares[i] = this.state.xIsNext ? 'X' : 'O';
-		this.setState({
-			history: history.concat([{
-				squares: squares
-			}]),
-			stepNumber: history.length,
-			xIsNext: !this.state.xIsNext,
-		});
-	}
-
-	jumpTo(step) {
-		this.setState({
-			stepNumber: step,
-			xIsNext: (step % 2) === 0
-		})
-	}
-
-	render() {
-		const history = this.state.history;
-		const current = history[this.state.stepNumber];
-		const winner = calculateWinner(current.squares);
-
-		const moves = history.map((step, move) => {
-			const desc = move ?
-				'Go to move #' + move :
-				'Go to game start';
-			return (
-				<li key={move}>
-					<button onClick={() => this.jumpTo(move)}>{desc}</button>
-				</li>
-			);
-		});
-
-		let status;
-		if (winner) {
-			status = 'Winner: ' + winner;
-		} else {
-			status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-		}
-
-		return (
-			<div className="game">
-				<div className="game-board">
-					<Board
-						squares={current.squares}
-						onClick={(i) => this.handleClick(i)}
-					/>
-				</div>
-				<div className="game-info">
-					<div>{status}</div>
-					<ol>{moves}</ol>
-				</div>
-			</div>
-		);
-	}
-}
-
-// ========================================
-
-function calculateWinner(squares) {
-	const lines = [
-		[0, 1, 2],
-		[3, 4, 5],
-		[6, 7, 8],
-		[0, 3, 6],
-		[1, 4, 7],
-		[2, 5, 8],
-		[0, 4, 8],
-		[2, 4, 6],
-	];
-	for (let i = 0; i < lines.length; i++) {
-		const [a, b, c] = lines[i];
-		if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-			return squares[a];
-		}
-	}
-	return null;
-}
-
-
-// ================= Actual Page =====================
 
 function Pokemon(props){
 	return (
 		<div class="pokemon">
 			<div class="pokemon-visual">
-				Bubusuru <br/><br/>
-				<img src="https://cdn.bulbagarden.net/upload/9/98/192Sunflora.png"/>
+				{props.data.name} <br/><br/>
+				<img src={props.data.img}/>
 			</div>
 			<div class="pokemon-stats">
 				<table>
-					<tr>
-						<td style={{width: "10em"}}> HP: </td>
-						<td style={{width: "3em"}}> {props.hp} </td>
-						<td> <div style={{backgroundColor: "#00f", width: `calc(100% * ${props.hp}/255)`, height: "15px"}}></div> </td>
-					</tr>
-					<tr>
-						<td style={{width: "10em"}}> Attack: </td>
-						<td style={{width: "3em"}}> {props.attack} </td>
-						<td> <div style={{backgroundColor: "#00f", width: `calc(100% * ${props.attack}/255)`, height: "15px"}}></div> </td>
-					</tr>
-					<tr>
-						<td style={{width: "10em"}}> Defense: </td>
-						<td style={{width: "3em"}}> {props.defense} </td>
-						<td> <div style={{backgroundColor: "#00f", width: `calc(100% * ${props.defense}/255)`, height: "15px"}}></div> </td>
-					</tr>
-					<tr>
-						<td style={{width: "10em"}}> Special Attack: </td>
-						<td style={{width: "3em"}}> {props.spattack} </td>
-						<td> <div style={{backgroundColor: "#00f", width: `calc(100% * ${props.spattack}/255)`, height: "15px"}}></div> </td>
-					</tr>
-					<tr>
-						<td style={{width: "10em"}}> Special Defense: </td>
-						<td style={{width: "3em"}}> {props.spdefense} </td>
-						<td> <div style={{backgroundColor: "#00f", width: `calc(100% * ${props.spdefense}/255)`, height: "15px"}}></div> </td>
-					</tr>
-					<tr>
-						<td style={{width: "10em"}}> Speed: </td>
-						<td style={{width: "3em"}}> {props.speed} </td>
-						<td> <div style={{backgroundColor: "#00f", width: `calc(100% * ${props.speed}/255)`, height: "15px"}}></div> </td>
-					</tr>
+					<Status_bar
+						title={"HP"}
+						value={props.data.stats.hp}
+					/>
+					<Status_bar
+						title={"Attack"}
+						value={props.data.stats.attack}
+					/>
+					<Status_bar
+						title={"Defense"}
+						value={props.data.stats.defense}
+					/>
+					<Status_bar
+						title={"Special Attack"}
+						value={props.data.stats.spattack}
+					/>
+					<Status_bar
+						title={"Special Defense"}
+						value={props.data.stats.spdefense}
+					/>
+					<Status_bar
+						title={"Speed"}
+						value={props.data.stats.speed}
+					/>
 				</table>
 			</div>
 		</div>
 	);
 }
 
-class App extends React.Component {
-
-	handleSubmit(data) {
-    alert(JSON.stringify(data));
-  }
-
-	render(){
+function PokeList(props){
+	const pokemons = props.data.map(pokemon => {
 		return(
-			<div class="app">
-				<h1> Pokesearch </h1>
-				<Search
-					onSubmit={(i) => this.handleSubmit(i)}/>
-				<br/>
-				<Pokemon
-					hp={200}
-					attack={100}
-					defense={10}
-					spattack={10}
-					spdefense={20}
-					speed={250}
-					/>
-				<br/>
-				<Pokemon
-					hp={50}
-					attack={20}
-					defense={200}
-					spattack={75}
-					spdefense={175}
-					speed={30}
-					/>
-			</div>
-		)
-	}
-
+			<>
+			<Pokemon
+				data ={pokemon}
+			/>
+			<br/>
+			</>
+		);
+	});
+	return(
+		<>{pokemons}</>
+	)
 }
 
 class Search extends React.Component {
@@ -247,9 +90,12 @@ class Search extends React.Component {
 	}
 
 	handleSubmit(event) {
-    this.props.onSubmit(this.state)
-    event.preventDefault();
-  }
+		event.preventDefault();
+		let params = ""
+		for (const [key, value] of Object.entries(this.state))
+			if(value) params += "&" + key + "=" + value
+		this.props.onSubmit(params)
+	}
 
 	render(){
 		return(
@@ -279,6 +125,43 @@ class Search extends React.Component {
 			</form>
 		)
 	}
+}
+
+class App extends React.Component {
+
+	constructor(props){
+		super(props);
+		this.state = {
+				data: null
+		};
+	}
+
+	handleSubmit(params) {
+		console.log("requesting http://localhost:3000/search?"+params)
+		let value = axios.get("http://localhost:3000/search?"+params).then(response => {
+			this.setState({data: response.status === 200 ? response.data : null});
+		}).catch(console.log);
+	}
+
+	render(){
+		return(
+			<div class="app">
+				<h1> Pokesearch </h1>
+				<Search
+					onSubmit={(i) => this.handleSubmit(i)}/>
+				<br/>
+				{
+					this.state.data ?
+						<PokeList
+							data={this.state.data}
+						/>
+						:
+						<h1> No Pokemon to Show</h1>
+				}
+			</div>
+		)
+	}
+
 }
 
 ReactDOM.render(
